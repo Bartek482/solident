c663f21dc7dace4e24e458dcdd3f7f5e3cf616bd
Bartek482 <log130@wp.pl>
2023-03-07 20:06:41 +0100

    Header - complete rework
    - html and scss complete rework
    - added hamburger menu for RWD

--- src/components/home components/HomeHeader.js
+++ src/components/home components/HomeHeader.js
@@ -4,22 +4,19 @@ import logo from "../../assets/solident.png"
 
 export function HomeHeader () {
     return (
-        <div className='home-header'>
-            <div className='home-header__container'>
+        <section className="top-nav">
+            <div>
                 <Link to='/'><img className='home-header__logo' src={logo} alt='solident' /></Link>
             </div>
-            <div className='home-header__container home-header__nav'>
-                <div className='home-header__language'>
-                    <Link className='header-button__hover button-active' to='/'>PL</Link>
-                    <p>/</p>
-                    <Link className='header-button__hover' to='/'>EN</Link>
-                </div>
-                <nav className='header-nav'>
-                    <Link className='header-button__hover' to='/'>Ełk</Link>
-                    <Link className='header-button__hover' to='/'>Wysokie Mazowieckie</Link>
-                    <Link className='header-button__hover' to='/'>Łomża</Link>
-                </nav>
-            </div>
-        </div>
+            <input id="menu-toggle" type="checkbox"/>
+            <label className='menu-button-container' htmlFor="menu-toggle">
+                <div className='menu-button'></div>
+            </label>
+            <ul className="menu">
+                <li><Link to='/'>Ełk</Link></li>
+                <li><Link to='/'>Wysokie Mazowieckie</Link></li>
+                <li><Link to='/'>Łomża</Link></li>
+            </ul>
+        </section>
     )
 }
